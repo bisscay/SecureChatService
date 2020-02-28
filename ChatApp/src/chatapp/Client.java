3613715bbbcd22b4b26585c8f6e0422420eb4b1d
grandpaRick <desmondumebeh@gmail.com>
2020-02-28 19:48:04 +0000

    generated the required challenge/random number on client A: (Pass A) performed hashing on random number: H(PassA)

--- ChatApp/src/chatapp/Client.java
+++ ChatApp/src/chatapp/Client.java
@@ -12,27 +12,39 @@ package chatapp;
 // Import Socket Class
 import java.net.Socket;
 // import OutputStream Class
-import java.io.OutputStream;
+//import java.io.OutputStream; // not utilized
 // import OutputStreamWriter Class
 import java.io.OutputStreamWriter;
 // import print writer
 import java.io.PrintWriter;
+import static java.lang.System.out;
 // import java security package
 import java.security.KeyPairGenerator;
 // import class to hold keys
 import java.security.KeyPair;
 // import
 import java.security.Key;
+// import java.net to use ipv4 address class
+import java.net.Inet4Address;
+//import for random number generation
+import java.security.SecureRandom;
+//import required to implement KeySpec.
+//Provides classes and interfaces for key specifications and algorithm parameter specifications.
+import java.security.spec.*;
+//Provides the classes and interfaces for cryptographic operations.
+//Import required to implement PBEKeySpec (Password based encryption)
+import javax.crypto.spec.*;
+//Provides the classes and interfaces for cryptographic operations.
+import javax.crypto.SecretKeyFactory;
 /**
  *
  * @author PUSER
  */
-public class Client {
-
+public class Client implements KeySpec { //
+    
     /**
      * @param args the command line arguments
      */
-    private final double PI = 3.242; 
     public static void main(String[] args) throws   Exception { // avoid throwing (Use try catch)
         // create keypair generator implementing RSA algorithm
         KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
@@ -49,37 +61,80 @@ public class Client {
         // TEST: see what pub & pvt hold
         System.out.println("Public Key: " +pub +"\nPublic Key in String fmt: " +str +"\nPrivate Key: " +pvt);
         
-        // State ip address / host name
-        String ip = "127.0.0.1";
+        //***********************************generate PassA***********************************************
+        //variable to identifier range for random generator
+        int rangeValue = 100000;
+        //create an instance of class SecureRandom
+        SecureRandom randNumber = new SecureRandom(); 
+        //PassA: random Number of type string
+        //generate a random nuber in range 0 to 99999 and convert to string
+        String PassA = Integer.toString(randNumber.nextInt(rangeValue));
+        
+        //*************************************************************************************************
+        
+        //**********************************Hashing(PassA)*************************************************
+        //generate salt for extendend hashing cryption
+        //this will retrive a random byte array.
+        SecureRandom random = new SecureRandom(); //create an instance of class SecureRandom
+        byte[] salt = new byte[16]; //creates a size 16 array of type byte
+        random.nextBytes(salt); //generate random byte and store in array(salt).
+        
+        KeySpec spec = new PBEKeySpec(PassA.toCharArray(), salt, 65536, 128); //
+        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");//converts key into key specification (high level cryptographic operation)
+        byte[] hash = factory.generateSecret(spec).getEncoded();
+        //H(PassA) completed
+        //*************************************************************************************************
+               
+        // State ip address (Same host but different port communication)
+        // (InetAddress class represents IP address) 
+        // because we are using a Socket ctsr that takes in 
+        // InetAddress address,
+        // int port, 
+        // InetAddress localAddr, and 
+        // int localPort
+        // Store host A address
+        // cast to ensure address is v4 and not v6
+        Inet4Address hostA = (Inet4Address)Inet4Address.getByName("localhost"); // catch UnknownHostException
+        /*
+        // Another Way, then you'd have to put in the values 127.0.0.1 in byte array 
+        byte [] test = new byte[4];
+        Inet4Address hostA = (Inet4Address)Inet4Address.getByAddress(test);
+        */
+        // test host A ip address value
+        //System.out.println("ip ADDDD: " + hostA);
+        // Store host B (same machine)
+        Inet4Address hostB = hostA; // TRY: Another networkcard on same Pc (Tease: Same pc can have multiple ip)
         // State port number
-        // Use a free port number 1024 - 65535
-        int port = 9999;
+        // random free port will be used for host A
+        int portA = 0;
+        // Use a free port number 1024 - 65535 for host B
+        int portB = 9999;
         
         // Open a Socket
         // Send Request to Server
         System.out.println("Client Up.");
-        Socket s = new Socket(ip,port);
+        // create a stream and connect to specified ip & port (Destination B)
+        // source of connection request can be specified as parameters
+        // same source and destination,
+        // select a free port 1024-65535 
+        // or select 0 for a random free port to be used
+        // or leave port as null, this checks if hostB is an anyLocal address 
+        // i.e address 0.0.0.0 (sockets that bind to all network cards)
+        Socket s = new Socket(hostB,portB,hostA,portA); // Catch IOException // what happens if port B is 0?
         System.out.println("Socket Request Sent");
-        OutputStreamWriter usher = new OutputStreamWriter(s.getOutputStream());
-        //usher.write(str);
+        // Following order of precedence
+        // create an output stream for the socket
+        // make the stream writable
+        OutputStreamWriter usher = new OutputStreamWriter(s.getOutputStream());// place in try with resources block
+        // flush the buffer on println() invocation 
+        // (this might be why only first line of public key is printed)
+        // Possible solution: alter what initiates flush, or
+        // take out carridge return and newline from sent public key string
         PrintWriter out = new PrintWriter(usher);
         out.println(str);
         out.flush();
         System.out.println("Message Sent");
         
-        /*
-        // Open output stream
-        OutputStream os = s.getOutputStream();
-        
-        // make writer(usher) aware of location to be written to
-        OutputStreamWriter usher = new OutputStreamWriter(os);
-        
-        // write to output stream
-        usher.write(str);
-        
-        // send request
-        os.flush();
-*/
         
     }
     
